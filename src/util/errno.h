5b6a0d11bebaea95f758cdb086cc180d65486f16
DonkeyCore <doomsdaysurvivor2013@gmail.com>
2015-11-01 12:38:22 -0600

    Created default errors

---
+++ src/util/errno.h
@@ -0,0 +1,12 @@
+#ifndef TWITCHOS_ERRNO_H
+#define TWITCHOS_ERRNO_H
+
+#define EPERM            1      /* Operation not permitted */
+#define EINTR            4      /* Interrupted system call */
+#define EAGAIN          11      /* Try again */
+#define EFAULT          14      /* Bad address */
+#define EINVAL          22      /* Invalid argument */
+#define EDOM		33	/* Math argument out of domain */
+#define ERANGE          34      /* Math result not representable */
+
+#endif //TWITCHOS_ERRNO_H
